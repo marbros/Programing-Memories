2c0aa67cde38d51949a04bc7e92290940a97b2e4
Mario <enigsea@gmail.com>
2013-10-15 23:40:04 -0500

    change in the order of the methods in the proyect.

--- Algorithm_Of_Huffman/Huffman.java
+++ Algorithm_Of_Huffman/Huffman.java
@@ -1,17 +1,15 @@
 /**
  * @author Mario Giraldo Restrepo
- * @deprecated Algorithm of Huffman
+ * @author Alejandro Taborda
  * @version 0.0.3
  */
 
-//http://www.eztigma.tk/juegos/binary.php
-
-import java.util.Scanner;
+//import java.util.Scanner;
 import java.util.TreeMap;
 import java.util.PriorityQueue;
 
 public class Huffman {
-
+    
     static Nodo treeD;
     static Nodo tree;
     private static String keys = "";
@@ -19,39 +17,17 @@ public class Huffman {
     private static Text t = new Text();
     private static String keyC = "";
     private static String Separator = "~";
-
-    public static void enOrden(Nodo arbol){
-        if (arbol != null){
-        	System.out.println("Nodo izq");
-            enOrden(arbol.getHijoIzq());
-            System.out.println("key " + arbol.getkey() + " valor " + arbol.getValue());
-            System.out.println("Nodo Der");
-            enOrden(arbol.getHijoDer());
-        }
-                System.out.println("---Fin---");
-    }	
-
-    public static void posOrden(Nodo arbol){
-        if (arbol != null){
-        	System.out.println("Nodo izq P");
-            enOrden(arbol.getHijoIzq());
-            System.out.println("Nodo der P");
-            enOrden(arbol.getHijoDer());
-            System.out.println("Valor en el nodo: "+arbol.getValue());
-        }
-    }
-    
-    /**
-	 *  Este método se encarga de retirar cada par de elementos de menor
-	 *  frecuencia en la cola, y sumar su peso con el fin de crear 
-	 *  pequeños subArboles, que seran nuevamente agregados a la cola
-	 *  hasta obtener un unico arbol.
-	 *
-	 *  @param cola   Cola de prioridades ordenada por frecuencia de 
-	 *				  cada caracter en el mapa.	 
-	 *  @param arbol  Es una instancia de la clase Arbol.
-	 */
-	private static void createTree(PriorityQueue cola, Arbol arbol, 
+        /**
+     *  Este método se encarga de retirar cada par de elementos de menor
+     *  frecuencia en la cola, y sumar su peso con el fin de crear 
+     *  pequeños subArboles, que seran nuevamente agregados a la cola
+     *  hasta obtener un unico arbol.
+     *
+     *  @param cola   Cola de prioridades ordenada por frecuencia de 
+     *                cada caracter en el mapa.  
+     *  @param arbol  Es una instancia de la clase Arbol.
+     */
+    private static void createTree(PriorityQueue cola, Arbol arbol, 
                 huffmanGui UI) {
 
             Nodo father = null; 
@@ -68,18 +44,18 @@ public class Huffman {
                 Nodo two = (Nodo) cola.poll();               
                 Integer onePlusTwo = one.getValue() + two.getValue(); 
                 father = Arbol.insertarNodo(onePlusTwo,one,two,father);
-                cola.add(father);    	
+                cola.add(father);       
             }
             tree = (Nodo)cola.peek();
             getKeys((Nodo)cola.poll());
-	}
+    }
 
-	/**
-	 *  Este método realiza un recorrido en orden de un arbol  
-	 *
-	 *  @param arbol  Es una instancia de la clase Nodo.
-	 *	@param key    Es el binario 0 ó 1 asociado a cada arco del arbol
-	 */
+    /**
+     *  Este método realiza un recorrido en orden de un arbol  
+     *
+     *  @param arbol  Es una instancia de la clase Nodo.
+     *  @param key    Es el binario 0 ó 1 asociado a cada arco del arbol
+     */
         public static void order(Nodo arbol, String key){
             keys += key;
             if (arbol != null){
@@ -89,27 +65,27 @@ public class Huffman {
                 order(arbol.getHijoDer(), "1");
             }
             keys = keys.substring(0,keys.length()-1);
-        }	
+        }
 
-	/**
-	 *  Este método realiza un recorrido a medias, en Pos Orden de un arbol,
-	 *  con el fin de asociar un valor binario a cada arco de este.
-	 *
-	 *  @param arbol  Es una instancia de la clase Nodo.
-	 */
-    public static void getKeys(Nodo arbol) {
-    	if(arbol != null) {
-    		order(arbol.getHijoIzq(), "0");
-            order(arbol.getHijoDer(), "1");   		
-    	}
+    /**
+     *  Este método realiza un recorrido a medias, en Pos Orden de un arbol,
+     *  con el fin de asociar un valor binario a cada arco de este.
+     *
+     *  @param arbol  Es una instancia de la clase Nodo.
+     */
+        public static String getKeys(Nodo arbol) {
+            if(arbol != null) {
+                order(arbol.getHijoIzq(), "0");
+                order(arbol.getHijoDer(), "1");         
+            }
+            return k;
+        }
 
-    }	
-
-	/**
-	 *  Este método imprime las claves asociadas a cada hoja del arbol.
-	 */
-	public static void printKeys(huffmanGui UI) {	
-		String Keys;
+    /**
+     *  Este método imprime las claves asociadas a cada hoja del arbol.
+     */
+    public static void printKeys(huffmanGui UI) {   
+        String Keys;
                 if(k.contains("\n")) {
                     Keys = k.replace("\n","º");
                     Keys = Keys.replace(Separator, "\n");
@@ -119,34 +95,34 @@ public class Huffman {
                 }
                 
                 UI.txaKeys.setText("-Keys- \n" + Keys);
-		System.out.println("-Keys- \n" + Keys);
+        System.out.println("-Keys- \n" + Keys);
                 t.write("claves.txt", Keys);
-	}
+    }
 
-	/**
-	 *  Esta función realiza un reemplazo de cada caracter del texto
-	 *  por su respectiva key asignada al arbol; luego imprime el nuevo
-	 *  texto resultante del anterior proceso. 
-	 *
-	 *	@param T  Texto
-	 */
-	public static String prinTextCompress(String T) {
-		char[] Keys;
+    /**
+     *  Esta función realiza un reemplazo de cada caracter del texto
+     *  por su respectiva key asignada al arbol; luego imprime el nuevo
+     *  texto resultante del anterior proceso. 
+     *
+     *  @param T  Texto
+     */
+    public static String prinTextCompress(String T) {
+        char[] Keys;
                 Keys = T.toCharArray();
-		char comp;
-		String keyComplete = "";
+        char comp;
+        String keyComplete = "";
                 String _k = k.replaceAll(" : ","");
-		for(int i = 0; i <= Keys.length-1; i++) {
-			comp = Keys[i]; 
-			if(Keys[i] == comp) {
+        for(int i = 0; i <= Keys.length-1; i++) {
+            comp = Keys[i]; 
+            if(Keys[i] == comp) {
                             int x = _k.indexOf(comp)+1;
                             keyComplete += _k.substring(_k.indexOf(comp)+1,
                                     _k.indexOf(Separator,_k.indexOf(comp)));
-			}
-		}
+            }
+        }
                 t.write("comprimido.txt", keyComplete);
                 return keyComplete;
-	}
+    }
 
     /**
      *  Esta Función convierte cada caracter del texto a un binario, luego
@@ -166,35 +142,35 @@ public class Huffman {
             keyBin += keyBinary[i] + " ";
         }
                 return keyBin;
-    }    		
+    }
 
-	/**
-     *  Este método crea el arbol de codificación huffman por medio de
-     *  métodos de apoyo, reemplazando cada caracter del texto a un binario,
-     *  después retorna el nuevo texto resultante del anterior proceso.
-	 *
-	 *	@param TC  Texto Comprimido
-     *  @param key Claves Del Texto
-	 */
-	public static String printTextDescompress(String TC, String key,
-                huffmanGui UI) {
-            Nodo root = new Nodo("",0);
-            key = key.replace(" : ", "");
-            String [] Keys;
-            Keys = key.split(Separator);
-            
-            String [] code;
-            code = TC.split("");
-            
-            for(int i = 0; i <= Keys.length-1; ++i) {
-                root = treeStruct(root,Keys[i]);
+    /**
+     *  Este método es una extencion de treeStruct que realiza el
+     *  recorrido de un arbol hasta llegar asus hojas.
+     *
+     *  @param root   Nodo del arbol actual
+         *      @param key[]  keys del texto comprimido
+         *      @param pos    Posición del char actual
+         *      @param len    Tamaño del arreglo key[]
+     */        
+        public static void orderT(Nodo root, char[] key, int pos, int len) {
+            if(pos <= len && len != 1) {
+                Nodo node = new Nodo("",0);
+                if(key[pos] == '0') {
+                    if(root.getHijoIzq() == null){
+                        root.setHijoIzq(node);
+                    }
+                    orderT(root.getHijoIzq(),key,pos+1,len);
+                }else {
+                    if(root.getHijoDer() == null){
+                        root.setHijoDer(node);                
+                    }
+                    orderT(root.getHijoDer(),key,pos+1,len);
+                }
             }
-            for(int i = 1; i < code.length-1; ++i) {
-                i = frecuence(root,code,0,i)-1;               
+            if(root.getHijoIzq() == null && root.getHijoDer() == null){
+                root.setkey(Character.toString(key[0]));                
             }
-            treeD = root;
-            UI.txaText.setText(keyC);
-            return keyC;
         }
 
         /**
@@ -227,14 +203,14 @@ public class Huffman {
             }
             keyC += root.getkey();
             return posf;
-        }         
+        } 
 
-    /**
+        /**
      *  Este método crea la estructura del arbol de huffman a partir de
      *  unas keys dadas.
      *
      *  @param root   Nodo raiz
-     *  @param key    keys del texto comprimido
+         *      @param key    keys del texto comprimido
      */
         public static Nodo treeStruct(Nodo root, String key){            
             char [] charac;
@@ -263,74 +239,74 @@ public class Huffman {
                 orderT(root.getHijoDer(),charac,pos,charac.length-1);                        
             }
             return root;
-        }        
-
-	/**
-	 *  Este método es una extencion de treeStruct que realiza el
-	 *  recorrido de un arbol hasta llegar asus hojas.
-	 *
-	 *	@param root   Nodo del arbol actual
-         *      @param key[]  keys del texto comprimido
-         *      @param pos    Posición del char actual
-         *      @param len    Tamaño del arreglo key[]
-	 */        
-        public static void orderT(Nodo root, char[] key, int pos, int len) {
-            if(pos <= len && len != 1) {
-                Nodo node = new Nodo("",0);
-                if(key[pos] == '0') {
-                    if(root.getHijoIzq() == null){
-                        root.setHijoIzq(node);
-                    }
-                    orderT(root.getHijoIzq(),key,pos+1,len);
-                }else {
-                    if(root.getHijoDer() == null){
-                        root.setHijoDer(node);                
-                    }
-                    orderT(root.getHijoDer(),key,pos+1,len);
-                }
+        }
+        
+    /**
+         *  Este método crea el arbol de codificación huffman por medio de
+         *  métodos de apoyo, reemplazando cada caracter del texto a un binario,
+         *  después retorna el nuevo texto resultante del anterior proceso.
+     *
+     *  @param TC  Texto Comprimido
+         *      @param key Claves Del Texto
+     */
+    public static String printTextDescompress(String TC, String key,
+                huffmanGui UI) {
+            Nodo root = new Nodo("",0);
+            key = key.replace(" : ", "");
+            String [] Keys;
+            Keys = key.split(Separator);
+            
+            String [] code;
+            code = TC.split("");
+            
+            for(int i = 0; i <= Keys.length-1; ++i) {
+                root = treeStruct(root,Keys[i]);
             }
-            if(root.getHijoIzq() == null && root.getHijoDer() == null){
-                root.setkey(Character.toString(key[0]));                
+            for(int i = 1; i < code.length-1; ++i) {
+                i = frecuence(root,code,0,i)-1;               
             }
-        }	
-
-	/**
-	 *  Esta función retorna un cola de prioridades ordenada por   
-	 *  frecuencia de cada caracter en el mapa.
-	 *
-	 *  @param map    Es el mapa de caracteres asociado con su frecuencia.
-	 *  @param arbol  Es una instancia de la clase Arbol
-	 *  @Retun cola
-	 */
-	private static PriorityQueue col_priority(TreeMap map, Arbol arbol) {
-            PriorityQueue<Nodo> cola = new PriorityQueue<>();			
+            treeD = root;
+            UI.txaText.setText(keyC);
+            return keyC;
+        }                          
+        
+    /**
+     *  Esta función retorna un cola de prioridades ordenada por   
+     *  frecuencia de cada caracter en el mapa.
+     *
+     *  @param map    Es el mapa de caracteres asociado con su frecuencia.
+     *  @param arbol  Es una instancia de la clase Arbol
+     *  @Retun cola
+     */
+    private static PriorityQueue col_priority(TreeMap map, Arbol arbol) {
+            PriorityQueue<Nodo> cola = new PriorityQueue<>();           
             for (Object key : map.descendingKeySet()) {
                 //int value = (int) map.get(key);
                 cola.add(new Nodo((String)key,(int)map.get(key)));
             }
             //System.out.println(cola.toString());          
             return cola;
-	}
-
-	/**
-	 *  Esta función retorna un map <k,v> con cada caracter  
-	 *  y el número de veces con que este se repite en un texto.
-	 *
-	 *  @param T     Es el Texto
+    }
+        
+    /**
+     *  Esta función retorna un map <k,v> con cada caracter  
+     *  y el número de veces con que este se repite en un texto.
+     *
+     *  @param T     Es el Texto
          *  @param type  Es el identificador de la función de llamado
-	 *  @Retun map
-	 */
-	private static TreeMap cont_Letters(String T, String type) {
+     *  @Retun map
+     */
+    private static TreeMap cont_Letters(String T, String type) {
         String[] letters;
         if(type.equals("letters")) {
             letters = T.split("");
         }else{
             letters = T.split(" ");                    
         }
-		int len = letters.length;
-		TreeMap <String, Integer> map = new TreeMap <>();
+        int len = letters.length;
+        TreeMap <String, Integer> map = new TreeMap <>();
 
-		for (int i=0; i < len; ++i) {
+        for (int i=0; i < len; ++i) {
                     String key = letters[i];
                     if (letters[i].length() > 0) {
                         if(map.get(key) == null) {
@@ -341,11 +317,11 @@ public class Huffman {
                             map.put(key,value);
                         }
                     }
-		}
-		
-		return map;	
-	}
-
+        }
+        
+        return map; 
+    }
+        
         public static void printText(String type, String Text, huffmanGui UI) {
             if(Text.length() > 47) {
                 UI.txaText.setText(UI.txaText.getText() + "\n" + "Texto " + type + ": " + Text + "\n");
@@ -356,28 +332,28 @@ public class Huffman {
                     UI.lblcTextDesCompress.setText(Text);
                 }
             }            
-        }    	
-    
-	/**
-	 *  Este método realiza la tarea de contenedor de cada funcíon y  
-	 *  método del programa y los ejecuta en un orden establecido.
-	 *
-	 *  @param T      Es el Texto
-	 *  @Retun arbol  Es una instancia de la clase Arbol.
-	 */
-	public static void huffman (String T, String K, Arbol arbol, 
+        }
+
+    /**
+     *  Este método realiza la tarea de contenedor de cada funcíon y  
+     *  método del programa y los ejecuta en un orden establecido.
+     *
+     *  @param T      Es el Texto
+     *  @Retun arbol  Es una instancia de la clase Arbol.
+     */
+    public static void huffman (String T, String K, Arbol arbol, 
                 huffmanGui UI, String action) {
             if(action.equals("Compress")) {
-		createTree(col_priority(cont_Letters(T,"letters"),arbol),arbol
+        createTree(col_priority(cont_Letters(T,"letters"),arbol),arbol
                         ,UI);
                 printKeys(UI);
                 String textCompress = prinTextCompress(T);
-		System.out.println("Texto Comprimido: " + textCompress);
+        System.out.println("Texto Comprimido: " + textCompress);
                 
                 printText("Comprimido",textCompress,UI);
                 
                 String textDesCompress = printTextDescompress(T);
-		System.out.println("Texto Sin Comprimir: " +
+        System.out.println("Texto Sin Comprimir: " +
                 printTextDescompress(T));
                 
                 printText("Sin Comprimir",textDesCompress,UI);
@@ -388,7 +364,7 @@ public class Huffman {
                 String comprenssion = printTextDescompress(T,K,UI);
                 System.out.println("Texto DesCifrado:   " + comprenssion);
             }  
-	}
+    }
 
 	/**
      *	Metodo de inicio.
