80792e3ba444cae36093f809f6c14e7c293e556a
Mario <enigsea@gmail.com>
2013-11-27 00:35:02 -0500

    the method 'rotar' is added (at last)

--- Fractals/Von_Koch_curve/Von_Koch.cpp
+++ Fractals/Von_Koch_curve/Von_Koch.cpp
@@ -1,5 +1,7 @@
+#include <cmath>
 #include namespace "miniwin.h"
 using namespace miniwin;
+using namespace std;
 
 //Curve Level 0 -
 //Curve Level 1 -^-
@@ -9,6 +11,16 @@ struct Punto
 	float x,  y;
 };
 
+//
+Punto rotar(const Punto& p, const Punto& cen, float da) {
+	float dx = p.x - cen.x, dy = p.y - cen.y;
+	float r = sqrt(dx*dx + dy*dy);
+	float a = atan2(dy, dx);
+	a += da;
+	Punto q = { cen.x + r * cos(a + da), cen.y + r * sin(a) };
+	return q;
+}
+
 void intermedio(const Punto& a, const Punto& b, float t) {
 	Punto p = { a.x * (1-t) + b.x * t, a.y * (1-t) + b.y };
 }
@@ -17,5 +29,6 @@ void Vonkoch(const Punto& p0, const Punto& p4, int nivel) {
 	Punto p[5] = { p0, p0, p0, p0, p4};
 	p[1] = intermedio(p[0], p[4], 1.0/3.0);
 	p[3] = intermedio(p[0], p[4], 2.0/3.0);
+	p[2] = rotar(p[3], p[1]. M_PI / 3.0);
 }
 
