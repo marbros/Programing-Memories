a3d417888610514dbd2337eec5cd2fe95b61ce13
Mario <enigsea@gmail.com>
2013-11-28 23:47:06 -0500

    the structure of proyect is added

---
+++ Big_Multiplication/x.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+struct Numero {
+	int cifras[30];
+	int ncifras;
+};
+
+int main() {
+	int tam;
+	string s;
+	Numero N;
+	cin >> s;
+	int tam = s.size();
+	for (int i = 0; i < tam; ++i) {
+		char digito = s[i];
+	}
+}
