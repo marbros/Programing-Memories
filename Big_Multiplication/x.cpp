9d9233f97ae9be796a9bcef512e96eb1a8d2bded
Mario <enigsea@gmail.com>
2013-11-29 00:11:29 -0500

    the structure of proyect is completed

--- Big_Multiplication/x.cpp
+++ Big_Multiplication/x.cpp
@@ -7,13 +7,27 @@ struct Numero {
 	int ncifras;
 };
 
-int main() {
-	int tam;
+void escribir_numero(const Numero& N) {
+	for (int i = N.ncifras - 1; i >= 0; --i) {
+		cout << N.cifras[i];
+	}	
+}
+
+void leer_numero(Numero& N) {
+	int tam;	
 	string s;
-	Numero N;
 	cin >> s;
 	int tam = s.size();
 	for (int i = 0; i < tam; ++i) {
 		char digito = s[i];
+		N.cifras[tam - 1 - i] = int(digito) - int('0');
 	}
+	N.ncifras = tam;
+}
+
+int main() {
+	Numero N;
+	leer_numero(N);
+	escribir_numero(N);
+	cout << endl;
 }
