2fa82f8b68a7ca8e32eb5e75fda76d4a524fcbf8
Mario <enigsea@gmail.com>
2014-01-27 02:19:41 -0500

    the structure of project is added

---
+++ Algorithm_Of_Bresenham/Bresenham.java
@@ -0,0 +1,30 @@
+package bresenham;
+
+/**
+ * @author Mario
+ */
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Bresenham extends JPanel {
+
+    public static JPanel panel ;    
+
+  public static void Bresenham(x1, y1, x2, y2) {
+    
+
+  }    
+    
+      
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Bresenham");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JOptionPane.showInputDialog("");
+        frame.setVisible(true);        
+    }
+}
+  
+
+
