5feb93f011fe33509facad05e217f2fc0285039a
Mario <enigsea@gmail.com>
2014-01-30 01:37:14 -0500

    the method 'init' is added

---
+++ Curves_Bezier/Curves_Bezier.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+	init();
+}
+
+var canvas, context;
+
+function init() {
+	var contenedor = document.getElementById("contenedor");
+
+	if(Modernizr.canvas) {
+		canvas = document.createElement("canvas");
+		canvas.width = 600;
+		canvas.height = 300;
+		contenedor.appendChild(canvas);
+
+		context = canvas.getContext("2d");
+
+		addListeners();
+	}else {
+		contenedor.innerHTML = "El navegador no soporta canvas";
+	}
+}
+
