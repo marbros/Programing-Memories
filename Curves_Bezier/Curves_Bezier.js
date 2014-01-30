78f2da2c94056ceb84557d0257fe94f8d59ebf19
Mario <enigsea@gmail.com>
2014-01-30 01:50:10 -0500

    the method 'paintQuad' is added

--- Curves_Bezier/Curves_Bezier.js
+++ Curves_Bezier/Curves_Bezier.js
@@ -2,7 +2,8 @@ window.onload = function() {
 	init();
 }
 
-var canvas, context;
+var canvas, context, prelastX, prelastY, lastX, lastY;
+var BprelastX, BprelastY, BlastX, BlastY;
 
 function init() {
 	var contenedor = document.getElementById("contenedor");
@@ -14,6 +15,8 @@ function init() {
 		contenedor.appendChild(canvas);
 
 		context = canvas.getContext("2d");
+		context.strokeStyle = "#foFF00";
+		context.lineWidth = 2;
 
 		addListeners();
 	}else {
@@ -22,6 +25,24 @@ function init() {
 }
 
 function paintQuad(e) {
+	if(lastX != null && prelastX !=y) {
+		context.beginPath();
+		context.moveTo(prelastX, prelastY);
+		context.quadraticCurveTo(lastX, lastY, e.layerX, e.layerY);
+		context.stroke();
+
+		prelastX = null;
+		prelastY = null;
+	}else if(lastX != null) {
+		prelastX = lastX;
+		prelastY = lastY;
+	}
+
+	lastX = e.layerX;
+	lastY = e.lastY;
+}
+
+function dibujaBez(e) {
 
 }
 
@@ -32,8 +53,10 @@ function addListener() {
 
 function mouseDown(e) {
 	paintQuad(e);
+	paintBez(e);
 }
 
 function touchDown(e) {
 	paintQuad(e);
+	paintBez(e);
 }
