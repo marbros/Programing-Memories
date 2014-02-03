0b702693464daf6abea9cfcb6d58303e01151972
Mario <enigsea@gmail.com>
2014-02-03 01:33:53 -0500

    the method 'coloR' is added

--- Algorithm_Of_Bresenham/Bresenham.java
+++ Algorithm_Of_Bresenham/Bresenham.java
@@ -33,12 +33,6 @@ public class Bresenham extends JPanel {
       int w =  size.width - insets.left - insets.right;
       int h =  size.height - insets.top - insets.bottom;
 
-      g2d.setColor(Color.BLUE);
-      // int x1 = Math.abs(r.nextInt()) % w;
-      // int y1 = Math.abs(r.nextInt()) % h;
-      // int x2 = Math.abs(r.nextInt()) % w;
-      // int y2 = Math.abs(r.nextInt()) % h;   
-
       int valx, valy, rad; // Ubicación y radio de cada circulo  
 
       for (int i = 0; i <= 1000 ; i++) {
@@ -46,6 +40,8 @@ public class Bresenham extends JPanel {
           rad = (int)(Math.random()*100);
           valx = (int)(Math.random()*w);
           valy = (int)(Math.random()*h);
+
+          coloR();
 
           int x,y,param;
           x = 0;
@@ -60,15 +56,41 @@ public class Bresenham extends JPanel {
                   y = y-1;
               }
               x++;
-              paintPoints(valx, valy, x, y);
+              paintPoints(valx, valy, x, y, g2d);
           }                   
       } 
 
-  }   
+  }
 
-  public static void paintPoints(x1, y1, x2, y2) {
-    
 
+  public static void coloR(Graphics2D g2d) {
+      Random rand = new Random();
+      float r , g , b;
+      r = g = b = rand.nextFloat();
+      Color rgb = new Color(r,g,b);
+         
+      //to get rainbow, pastel colors
+      Random random = new Random();
+      final float hue = random.nextFloat();
+      final float saturation = 1.0f;//1.0 for brilliant, 0.0 for dull
+      final float luminance = 9.9f; //1.0 for brighter, 0.0 for black
+      rgb = Color.getHSBColor(hue, saturation, luminance);          
+      g2d.setColor(rgb);      
+  }     
+
+  public static void paintPoints(int valx, int valy, int x, int y, Graphics2D g2d) {
+    //x1, y1, x2, y2
+    g2d.drawLine(valx + x, valy + y, valx + x, valy + y); //(x,y)
+    g2d.drawLine(valx - x, valy - y, valx - x, valy - y); //(-x,-y)
+
+    g2d.drawLine(valx + y, valy + x, valx + y, valy + x); //(y,x)
+    g2d.drawLine(valx - y, valy - x, valx - y, valy - x); //(-y,-x)
+
+    g2d.drawLine(valx + x, valy - y, valx + x, valy - y); //(x,-y)
+    g2d.drawLine(valx - x, valy + y, valx - x, valy + y); //(-x,y)
+
+    g2d.drawLine(valx + y, valy - x, valx + y, valy - x); //(y,-x)
+    g2d.drawLine(valx - y, valy + x, valx - y, valy + x); //(-y,x)    
   }    
     
       
